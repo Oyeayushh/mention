7a1d49987a3d4b3c1e12aaebecaec7dbcb93ab0d
𝐀𝐘𝐔𝐒𝐇 𝐊𝐔𝐌𝐀𝐑 𝐘𝐀𝐃𝐀𝐕 <pragati.prajapati2009@gmail.com>
2026-04-24 12:04:41 +0530

    Update __main__.py

--- AIMentionBot/__main__.py
+++ AIMentionBot/__main__.py
@@ -3,6 +3,7 @@ from pyrogram import idle
 from AIMentionBot import app
 from AIMentionBot.database import db
 
+
 async def main():
     await db.connect()
     await app.start()
@@ -10,5 +11,5 @@ async def main():
     await idle()
     await app.stop()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+
+app.run(main())
